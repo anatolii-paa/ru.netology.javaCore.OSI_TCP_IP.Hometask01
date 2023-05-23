e44d7d5a3b2fd0e0a922e54632982cf266b81f94
anatolii-paa <anatolii.paa@gmail.com>
2023-05-23 09:11:00 +0400

    HomeTask01 about client-server interaction

---
+++ src/main/java/ru/netology/Main.java
@@ -0,0 +1,10 @@
+package ru.netology;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //Task about client-server interaction
+        //Contains two modules: Client.java and Server.java
+
+    }
+}
